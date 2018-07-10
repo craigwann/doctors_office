53afc417326b318701e33373c456a192ad39d189
Craig Wann and Eric Shull <eric@>
2018-07-10 13:56:10 -0700

    database file structure

---
+++ database.rb
@@ -0,0 +1,7 @@
+Guest=# CREATE DATABASE doctor;
+Guest=# \c doctor
+doctor=# CREATE TABLE doctors (id serial PRIMARY KEY, name varchar, specialty varchar);
+doctor=# CREATE TABLE patients (id serial PRIMARY KEY, name varchar, birthdate date);
+doctor=# ALTER TABLE patients ADD dr_id int;
+doctor=# ALTER TABLE patients ADD FOREIGN KEY (dr_id) REFERENCES doctors (id);
+doctor=# CREATE DATABASE doctor_test with template doctor;
